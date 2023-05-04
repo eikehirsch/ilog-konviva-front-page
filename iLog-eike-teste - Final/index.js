0e68b3c60c780029c967fdb2e3ff5fe970426787
Eike Hirsch <108685558+eikehirsch@users.noreply.github.com>
2023-05-04 10:54:25 -0300

    Uploading full project

---
+++ iLog-eike-teste - Final/index.js
@@ -0,0 +1,380 @@
+//#region courses mock data
+const courseData = [
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "1 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "2 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "3 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "4 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "5 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "6 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "7 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "8 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "9 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "10 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "11 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "12 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Curso",
+  },
+];
+
+const trailData = [
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "1 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "2 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "3 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "4 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "5 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "6 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "7 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "8 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "9 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "10 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "11 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+  {
+    imgSrc: "assets/img/card-image-1.png",
+    Title: "12 - Transformando comportamentos em tempos de mudança",
+    Duration: "20h 30min",
+    Description: "Desenvolvimento Pessoal e Profissional",
+    Rating: 3.5,
+    evaluationsCount: "(+100)",
+    evaluationPoints: 20000,
+    tag: "Trilha",
+  },
+];
+
+//#endregion
+
+$(document).ready(function () {
+  populateCarousel();
+});
+
+function populateCarousel() {
+  let coursesInner = $("#carousel-courses-inner");
+  let trailsInner = $("#carousel-trails-inner");
+
+  appendCards(courseData, coursesInner);
+  appendCards(trailData, trailsInner);
+
+  let coursesWidth = $("#carousel-courses-inner")[0].scrollWidth;
+  let coursesScrollPosition = 0;
+  let coursesNext = $("#carousel-courses-next");
+  let coursesPrev = $("#carousel-courses-prev");
+
+  let trailsWidth = $("#carousel-trails-inner")[0].scrollWidth;
+  let trailsScrollPosition = 0;
+  let trailsNext = $("#carousel-trails-next");
+  let trailsPrev = $("#carousel-trails-prev");
+
+  setCarouselEvents(
+    coursesWidth,
+    coursesScrollPosition,
+    coursesInner,
+    coursesNext,
+    coursesPrev,
+    courseData.length
+  );
+
+  setCarouselEvents(
+    trailsWidth,
+    trailsScrollPosition,
+    trailsInner,
+    trailsNext,
+    trailsPrev,
+    5
+  );
+}
+
+function setCarouselEvents(
+  carouselWidth,
+  scrollPosition,
+  carouselInner,
+  carouselNext,
+  carouselPrev,
+  cardCount
+) {
+  let cardWidth = $(".carousel-item").width();
+
+  carouselNext.on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 5) {
+      scrollPosition += cardWidth;
+      carouselInner.animate({ scrollLeft: scrollPosition }, 600); //scroll left
+    }
+  });
+
+  carouselPrev.on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      carouselInner.animate({ scrollLeft: scrollPosition }, 600);
+    }
+  });
+}
+
+function appendCards(data, innerTemplate) {
+  $.each(data, function (i) {
+    let templateString = `<div class="carousel-item">
+        <div class="card">
+          <img src="${data[i].imgSrc}" alt="Card Image" />
+          <div class="tag-course">${data[i].tag}</div>
+          <div class="card-body">
+            <span class="card-title"   data-toggle="tooltip"
+            data-placement="top"
+            title="${data[i].Title}">
+            ${data[i].Title}"
+            </span>
+            <div class="card-description">
+              <span class="card-hour">${data[i].Duration}"</span>
+              <i class="bi bi-dot card-description-separator"></i>
+              <span class="card-description-text"
+                >${data[i].Description}"</span
+              >
+            </div>
+            <div class="card-description-evaluations">
+              <img
+                class="card-description-stars"
+                src="assets/svg/filled-star.svg"
+                alt="Estrela cheia"
+              />
+              <img
+                class="card-description-stars"
+                src="assets/svg/half-star.svg"
+                alt="Meia estrela"
+              />
+              <img
+                class="card-description-stars"
+                src="assets/svg/empty-star.svg"
+                alt="Estrela vazia"
+              />
+              <img
+                class="card-description-stars"
+                src="assets/svg/empty-star.svg"
+                alt="Estrela vazia"
+              />
+              <img
+                class="card-description-stars"
+                src="assets/svg/empty-star.svg"
+                alt="Estrela vazia"
+              />
+              <span class="evaluations-reviews">${data[i].Rating} ${data[i].evaluationsCount}</span>
+              <i class="bi bi-dot card-description-separator"></i>
+              <span class="evaluations-points">${data[i].evaluationPoints}</span>
+            </div>
+            <div class="card-button-container">
+            <a
+                href="https://www.konviva.com.br/treinamento-e-desenvolvimento/"
+                target="_blank"
+                
+              <button class="card-button-more-info">
+                MAIS INFORMAÇÕES
+              </button>
+              </a
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+    innerTemplate.append(templateString);
+  });
+}
